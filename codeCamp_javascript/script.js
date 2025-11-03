e9f4c5070039de77f62cfe9a97730a4e03dcc8ab
kyhslam <kyhslam@gmail.com>
2025-11-03 13:05:11 +0900

    [2025-11-03] 코드캡프 javascript 학습

--- codeCamp_javascript/script.js
+++ codeCamp_javascript/script.js
@@ -1,12 +1,30 @@
+
+const todoList = document.querySelector('#todo-list');
+const todoInput = document.querySelector('#todo-input');
+
+const createTodo = function() {
+    let inputValue = document.querySelector('#todo-input').value;
+    const newLi = document.createElement('li');
+    const newSpan = document.createElement('span');
+    const newBtn = document.createElement('button');
+
+    newBtn.addEventListener('click', () => {
+        newLi.classList.toggle('complete');
+    })
+
+
+    newSpan.textContent = inputValue;
+    newLi.appendChild(newBtn);
+    newLi.appendChild(newSpan);
+    todoList.appendChild(newLi);
+    console.log(newLi);
+    todoInput.value = '';
+}
+
+
 const keyCodeCheck = function() {
-    //console.log(window.event)
-
     if (window.event.keyCode === 13) {
-        const inputValue = document.querySelector('#todo-input');
-        console.log(inputValue);
-
-        const hiTag = document.querySelector('#hiTag');
-        hiTag.textContent = 'test Val';
+        createTodo();
     }
 }
 
