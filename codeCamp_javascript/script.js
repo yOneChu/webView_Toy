6e6b1f3094bc68e4bdd2e90ef8e44d0fff448923
kyhslam <kyhslam@gmail.com>
2025-11-05 10:46:17 +0900

    [2025-11-05] 코드캡프 javascript 학습

--- codeCamp_javascript/script.js
+++ codeCamp_javascript/script.js
@@ -3,6 +3,7 @@ const todoList = document.querySelector('#todo-list');
 const todoInput = document.querySelector('#todo-input');
 
 const createTodo = function() {
+    console.log('--- createTodo ---');
     let inputValue = document.querySelector('#todo-input').value;
     const newLi = document.createElement('li');
     const newSpan = document.createElement('span');
@@ -12,13 +13,21 @@ const createTodo = function() {
         newLi.classList.toggle('complete');
     })
 
+    // 더블클릭
+    newLi.addEventListener('dblclick', () => {
+        newLi.remove();
+    })
+
 
     newSpan.textContent = inputValue;
     newLi.appendChild(newBtn);
     newLi.appendChild(newSpan);
     todoList.appendChild(newLi);
-    console.log(newLi);
+    //console.log(newLi);
     todoInput.value = '';
+    console.log(todoList);
+    //console.log(todoList.children[0].querySelector('span').textContent);
+    saveItems()
 }
 
 
@@ -29,4 +38,25 @@ const keyCodeCheck = function() {
 }
 
 
+const deleteAll = function() {
+    const  liList = document.querySelectorAll('li');
+    for( let i = 0; i < liList.length; i++ ) {
+        liList[i].remove();
+    }
+}
+
+const saveItems = function() {
+    const saveItems = [];
+    console.log(todoList.children[0].querySelector('span').textContent);
+    for (let i = 0; i < todoList.children.length; i++) {
+        //const todo = todoList.children[i].querySelector('span').textContent;
+        const todoObj = {
+            contents : todoList.children[i].querySelector('span').textContent,
+            complete : todoList.children[i].classList.contains('complete')
+        }
+        saveItems.push(todoObj);
+    }
+    console.log(saveItems);
+
+}
 
